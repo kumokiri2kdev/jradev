e8de943bc5373f3077db28ccc9d1727fc9d646f0
Seiichi Kataoka <seiichikataoka@Seiichi-no-iMac.local>
2017-09-10 07:57:11 +0900

    [mod]Add parameter specifying test file path to test app

--- jra/parser_test.py
+++ jra/parser_test.py
@@ -1,8 +1,15 @@
 import parser_top as pr
+import sys
+
+args = sys.argv
+
+if len(args) < 2:
+    print("Specify Test File Name")
+    sys.exit()
 
 _parser = pr.parser_top()
 
 
-with open('jra_top.html','rb') as rfp:
+with open(args[1],'rb') as rfp:
 	response_body = rfp.read().decode("'shift_jis'")
 	_parser.parse(response_body)
