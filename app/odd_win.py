1953fe2c76c3e27e990aa2b43d668e89e42996b6
Seiichi Kataoka <seiichikataoka@Seiichi-no-iMac.local>
2017-09-16 19:31:22 +0900

    Add app for checking "win" simply

---
+++ app/odd_win.py
@@ -0,0 +1,37 @@
+import sys
+
+sys.path.append('../')
+
+from jra import parser_top as pr
+from jra import parser_odds_top as prot
+from jra import parser_odds_kaisai as prok
+from jra import parser_odds_win as prow
+
+parser_top = pr.parser_top()
+param_list = parser_top.parse()
+
+if 'odds' in param_list == False:
+    print("No odd param")
+    sys.exit()
+
+parser_odds_top = prot.parser_odds_top('/JRADB/accessO.html', param_list['odds'])
+kaisai_list = parser_odds_top.parse()
+
+for kaisai in kaisai_list:
+    print("日付 : {}".format(kaisai['date']))
+    kaisai_place_list = kaisai['kaisai_list']
+    for kaisai_place in kaisai_place_list:
+        print(" - 開催 : {}".format(kaisai_place['kaisai']))
+        parser_odds_kaisai = prok.parser_odds_kaisai('/JRADB/accessO.html', kaisai_place['param'])
+        race_list = parser_odds_kaisai.parse()
+        for race in race_list:
+            print("  -- {} {}".format(race['no'], race['condition']))
+            parser_win = prow.parser_odds_win('/JRADB/accessO.html', race['win'])
+            uma_list =  parser_win.parse()
+            for uma_data in uma_list:
+                print("   [{}]{} {} {} ({} - {})".format(uma_data['number'], uma_data['uma']['name'], 
+                                                                    uma_data['sexage'], uma_data['odds']['win'],
+                                                                    uma_data['odds']['fuku_min'], uma_data['odds']['fuku_max']))
+
+
+        
