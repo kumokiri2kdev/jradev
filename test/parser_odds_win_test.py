1a4dd24dc684c18c83c9a826976f423e8691c752
Seiichi Kataoka <seiichikataoka@Seiichi-no-iMac.local>
2017-09-23 13:27:40 +0900

    [fix]Handle no weight info case

--- test/parser_odds_win_test.py
+++ test/parser_odds_win_test.py
@@ -9,7 +9,7 @@ from jra import parser_odds_win as prow
 
 use_network = True
 
-_parser = prow.parser_odds_win('/JRADB/accessO.html', 'pw151ouS306201704031120170916Z/B8')
+_parser = prow.parser_odds_win('/JRADB/accessO.html', 'pw151ouS306201704071120170924Z/64')
 
 if use_network:
    uma_list =  _parser.parse()
@@ -29,8 +29,10 @@ for uma_data in uma_list:
     print("  url : {}".format(uma_data['uma']['url']))
     print("  param : {}".format(uma_data['uma']['param']))
     print(" 単勝 : {}, 複勝 : {} - {}".format(uma_data['odds']['win'], uma_data['odds']['fuku_min'], uma_data['odds']['fuku_max']))
-    print(" 馬体重 : {}".format(uma_data['weight']))
-    print(" 増減 : {}".format(uma_data['diff']))
+    if 'weight' in uma_data:
+        print(" 馬体重 : {}".format(uma_data['weight']))
+        print(" 増減 : {}".format(uma_data['diff']))
+
     print(" {} {}".format(uma_data['jokey']['name'], uma_data['sexage']))
     print("    url : {}".format(uma_data['jokey']['url']))
     print("    param : {}".format(uma_data['jokey']['param']))
