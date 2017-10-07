27bd08ac0022cef995aa80de01fbb2a962ebdc58
kumokiri2k <kumokiri2k@kumokiri2knoMacBook.local>
2017-10-07 20:20:09 +0900

    [mod]Change final odds judge logic

--- jra/parser_odds.py
+++ jra/parser_odds.py
@@ -7,27 +7,30 @@ class parser_odds(prp.parser_post):
     def parse_content(self, soup):
         fixed = True
         div = soup.find('div', attrs={'class' : 'raceTtlTable'})
+        odds_info = {}
         if div:
             spans = div.find_all('span')
             for span in spans:
                 if span.has_attr('class'):
                     if 'headerOdds2' in span['class']:
                         value = span.get_text()
-                        if "現在" in value:
+                        if not "最終" in value:
                             try:
                                 time_value = pu.func_parser.parse_time(value)
                                 full, stamp = pu.parser_util_convert_timestr_full_and_stamp(time_value)
                                 fixed = False
+                                odds_info['log'] = 'Value OK 0001'
                                 break
                             except:
-                                print("ValueError : {}".format(value))
-                                odds_info['log'] = 'Value Error 0001'
+                                print("ValueError : {}".format(span.get_text()))
+                                odds_info['log'] = 'Value Error 0002 : {}'.format(span.get_text())
                         else:
                             print(">> 最終オッズ")
+                            odds_info['log'] = 'Value OK 0002'
                     else:
                         print(">>> 最終オッズ")
+                        odds_info['log'] = 'Value OK 0003'
 
-        odds_info = {}
         odds_info['fixed'] = fixed
         if fixed == False:
             odds_info['timestamp'] = stamp
