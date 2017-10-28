452ce04dfe6e9b403f423f9e6bcb5fcc83f503d3
Seiichi Kataoka <seiichikataoka@Seiichi-no-iMac.local>
2017-10-28 11:18:46 +0900

    [mod]Handle 1 or 2 days before fixed odds

--- jra/parser_odds.py
+++ jra/parser_odds.py
@@ -1,11 +1,20 @@
 from . import parser as pr
 from . import parser_util as pu
 from . import parser_post as prp
+from enum import Enum
+
+class odds_status(Enum):
+    WORKING = 1
+    FIXED = 2
+    FIXED_1DAY_BEFORE = 3
+    FIXED_2DAY_BEFORE = 4
+    UNKNOWN = 99
 
 class parser_odds(prp.parser_post):
 
     def parse_content(self, soup):
-        fixed = True
+        status = odds_status.UNKNOWN
+
         div = soup.find('div', attrs={'class' : 'raceTtlTable'})
         odds_info = {}
         if div:
@@ -18,7 +27,7 @@ class parser_odds(prp.parser_post):
                             try:
                                 time_value = pu.func_parser.parse_time(value)
                                 full, stamp = pu.parser_util_convert_timestr_full_and_stamp(time_value)
-                                fixed = False
+                                status = odds_status.WORKING
                                 odds_info['log'] = 'Value OK 0001'
                                 break
                             except:
@@ -26,15 +35,28 @@ class parser_odds(prp.parser_post):
                                 odds_info['log'] = 'Value Error 0002 : {}'.format(span.get_text())
                         else:
                             print(">> 最終オッズ")
+                            if "前々日" in value:
+                                status = odds_status.FIXED_2DAY_BEFORE
+                                stamp = 9999999997.9
+                            elif "前日" in value:
+                                status = odds_status.FIXED_1DAY_BEFORE
+                                stamp = 9999999998.9
+                            else:
+                                status = odds_status.FIXED
+                                stamp = 9999999999.9
+
                             odds_info['log'] = 'Value OK 0002'
                     else:
                         print(">>> 最終オッズ")
                         odds_info['log'] = 'Value OK 0003'
 
-        odds_info['fixed'] = fixed
-        if fixed == False:
-            odds_info['timestamp'] = stamp
+        odds_info['timestamp'] = stamp
+
+        if status != odds_status.FIXED:
+            odds_info['fixed'] = False
             #print("{} 現在のオッズ({})".format(time_value, full))            
+        else:
+            odds_info['fixed'] = True
 
         odds_list = self.parse_odds_content(soup)
         odds_info['odds'] = odds_list
