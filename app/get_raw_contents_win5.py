c32b03d7362ec28de4ba10898da1f53a2a1a1472
kumokiri2k <kumokiri2k@kumokiri2knoMacBook.local>
2018-03-10 11:23:57 +0900

    [mod]Use util function

--- app/get_raw_contents_win5.py
+++ app/get_raw_contents_win5.py
@@ -1,27 +1,22 @@
 import sys
 sys.path.append('../')
 
-import urllib.request
 from jra import parser_win5_kaisai as prwk
+from jra import parser_util as pu
 import re
-
-
-def cache_contents(param):
-  cname = "cname={}".format(param).encode('utf-8')
-  request = urllib.request.Request('http://www.jra.go.jp/JRADB/access5.html', data=cname, method='POST')
-
-  param = param.replace('/','-')
-  with urllib.request.urlopen(request) as response:
-    response_body = response.read()
-    with open("/Users/seiichikataoka/Documents/development/jra/testdata/data/" + param,'wb') as wfp:
-      wfp.write(response_body)
 
 
 param = 'pw17hde01201104241/18'
 
 while True:
-    cache_contents(param)
+    pu.parser_util_cache_contents('/access5.html', param)
     _parser = prwk.parser_win5_kaisai('/JRADB/access5.html',param)
-    param = _parser.parse()
-    if param == None:
+    win5 = _parser.parse()
+    if win5 == None:
         break;
+
+    if ('next' in win5 == False):
+        break
+
+    print(win5['date'])
+    param = win5['next']
